9d960de153f184af8507853bd3378bce513a8c42
brandonw504 <brandonw504@outlook.com>
2024-10-30 00:33:21 +0000

    Multithreaded but without desired behavior

--- project3/gunrock.cpp
+++ project3/gunrock.cpp
@@ -9,7 +9,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <deque>
+#include <queue>
 
 #include "HTTPRequest.h"
 #include "HTTPResponse.h"
@@ -22,6 +22,10 @@
 
 using namespace std;
 
+pthread_cond_t mainCond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t workerCond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 int PORT = 8080;
 int THREAD_POOL_SIZE = 1;
 int BUFFER_SIZE = 1;
@@ -30,6 +34,8 @@ string SCHEDALG = "FIFO";
 string LOGFILE = "/dev/null";
 
 vector<HttpService *> services;
+queue<MySocket *> buffer;
+int numRequestsProcessing = 0;
 
 HttpService *find_service(HTTPRequest *request) {
    // find a service that is registered for this path prefix
@@ -60,48 +66,65 @@ void invoke_service_method(HttpService *service, HTTPRequest *request, HTTPRespo
   }
 }
 
-void handle_request(MySocket *client) {
-  HTTPRequest *request = new HTTPRequest(client, PORT);
-  HTTPResponse *response = new HTTPResponse();
-  stringstream payload;
-  
-  // read in the request
-  bool readResult = false;
-  try {
-    payload << "client: " << (void *) client;
-    sync_print("read_request_enter", payload.str());
-    readResult = request->readRequest();
-    sync_print("read_request_return", payload.str());
-  } catch (...) {
-    // swallow it
-  }    
+void* handle_request(void *args) {
+  while (true) {
+    dthread_mutex_lock(&lock);
+    while (buffer.empty()) {
+      dthread_cond_wait(&workerCond, &lock);
+    }
+
+    MySocket *client = buffer.front();
+    buffer.pop();
+    numRequestsProcessing++;
+
+    HTTPRequest *request = new HTTPRequest(client, PORT);
+    HTTPResponse *response = new HTTPResponse();
+    stringstream payload;
     
-  if (!readResult) {
-    // there was a problem reading in the request, bail
+    // read in the request
+    bool readResult = false;
+    try {
+      payload << "client: " << (void *) client;
+      sync_print("read_request_enter", payload.str());
+      readResult = request->readRequest();
+      sync_print("read_request_return", payload.str());
+    } catch (...) {
+      // swallow it
+    }    
+      
+    if (!readResult) {
+      // there was a problem reading in the request, bail
+      delete response;
+      delete request;
+      sync_print("read_request_error", payload.str());
+      return NULL;
+    }
+    
+    HttpService *service = find_service(request);
+    invoke_service_method(service, request, response);
+
+    // send data back to the client and clean up
+    payload.str(""); payload.clear();
+    payload << " RESPONSE " << response->getStatus() << " client: " << (void *) client;
+    sync_print("write_response", payload.str());
+    cout << payload.str() << endl;
+    client->write(response->response());
+      
     delete response;
     delete request;
-    sync_print("read_request_error", payload.str());
-    return;
+
+    payload.str(""); payload.clear();
+    payload << " client: " << (void *) client;
+    sync_print("close_connection", payload.str());
+    client->close();
+    delete client;
+
+    numRequestsProcessing--;
+    dthread_cond_signal(&mainCond);
+    dthread_mutex_unlock(&lock);
   }
-  
-  HttpService *service = find_service(request);
-  invoke_service_method(service, request, response);
 
-  // send data back to the client and clean up
-  payload.str(""); payload.clear();
-  payload << " RESPONSE " << response->getStatus() << " client: " << (void *) client;
-  sync_print("write_response", payload.str());
-  cout << payload.str() << endl;
-  client->write(response->response());
-    
-  delete response;
-  delete request;
-
-  payload.str(""); payload.clear();
-  payload << " client: " << (void *) client;
-  sync_print("close_connection", payload.str());
-  client->close();
-  delete client;
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
@@ -139,16 +162,35 @@ int main(int argc, char *argv[]) {
 
   sync_print("init", "");
   MyServerSocket *server = new MyServerSocket(PORT);
-  MySocket *client;
 
   // The order that you push services dictates the search order
   // for path prefix matching
   services.push_back(new FileService(BASEDIR));
+
+  // Create thread pool
+  vector<pthread_t> threadPool (THREAD_POOL_SIZE);
+  for (int i = 0; i < THREAD_POOL_SIZE; i++) {
+    int ret =  dthread_create(&threadPool[i], NULL, &handle_request, NULL);
+    if (ret != 0) {
+      cout << "Error: failed to create thread" << endl;
+      exit(1);
+    }
+  }
   
   while(true) {
+    dthread_mutex_lock(&lock);
+    while (static_cast<int>(buffer.size()) + numRequestsProcessing >= BUFFER_SIZE) {
+      dthread_cond_wait(&mainCond, &lock);
+    }
+
     sync_print("waiting_to_accept", "");
-    client = server->accept();
+    buffer.push(server->accept());
     sync_print("client_accepted", "");
-    handle_request(client);
+    
+    if (!buffer.empty()) {
+      dthread_cond_signal(&workerCond);
+    }
+
+    dthread_mutex_unlock(&lock);
   }
 }
